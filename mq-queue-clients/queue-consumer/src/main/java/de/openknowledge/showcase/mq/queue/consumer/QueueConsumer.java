1e3247066b1b9121fa4d13c075701612582d1a42
max.weis <max.weis@openknowledge.de>
2021-06-16 09:10:49 +0200

    Add AsyncAPI Annotation and Document

--- mq-queue-clients/queue-consumer/src/main/java/de/openknowledge/showcase/mq/queue/consumer/QueueConsumer.java
+++ mq-queue-clients/queue-consumer/src/main/java/de/openknowledge/showcase/mq/queue/consumer/QueueConsumer.java
@@ -15,28 +15,77 @@
  */
 package de.openknowledge.showcase.mq.queue.consumer;
 
+import io.smallrye.asyncapi.spec.annotations.channel.ChannelItem;
+import io.smallrye.asyncapi.spec.annotations.message.Message;
+import io.smallrye.asyncapi.spec.annotations.operation.Operation;
+import io.smallrye.asyncapi.spec.annotations.schema.Schema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ejb.MessageDriven;
 import javax.interceptor.Interceptors;
 import javax.jms.JMSException;
-import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 import javax.json.bind.JsonbBuilder;
 
+import io.smallrye.asyncapi.spec.annotations.AsyncAPI;
+import io.smallrye.asyncapi.spec.annotations.info.Info;
+import io.smallrye.asyncapi.spec.annotations.info.License;
+import io.smallrye.asyncapi.spec.annotations.server.Server;
+import io.smallrye.asyncapi.spec.annotations.server.ServerVariable;
+
 /**
  * JMS consumer that receives messages from a queue. The queue is configured in the server.xml.
  */
+@AsyncAPI(
+    asyncapi = "2.0.0",
+    defaultContentType = "plain/text",
+    info = @Info(
+        title = "JMS Queue Consumer API",
+        version = "0",
+        description = "A Java EE App which receives messages via JMS from a MQ Broker",
+        license = @License(
+            name = "Apache 2.0",
+            url = "https://www.apache.org/licenses/LICENSE-2.0"
+        )
+    ),
+    id = "urn:de:openknowledge:queue:consumer:server",
+    servers = {
+        @Server(
+            url = "localhost:{port}",
+            protocol = "jms",
+            description = "Test broker",
+            name = "dev",
+            variables = {
+                @ServerVariable(
+                    name = "port",
+                    description = "Connection is available through port 1414.",
+                    defaultValue = "1414"
+                )
+            }
+        )
+    }
+)
 @Interceptors(TracingInterceptor.class)
 @MessageDriven
 public class QueueConsumer implements MessageListener {
 
   private static final Logger LOG = LoggerFactory.getLogger(QueueConsumer.class);
 
+  @ChannelItem(
+      channel = "DEV.QUEUE.MESSAGES.1",
+      subscribe = @Operation(
+          description = "Receive a message from the MQ Broker",
+          operationId = "onMessage",
+          message = @Message(
+              name = "Custom Message",
+              payload = @Schema(ref = "#/components/schemas/CustomMessage")
+          )
+      )
+  )
   @Override
-  public void onMessage(final Message message) {
+  public void onMessage(final javax.jms.Message message) {
     try {
       String textMessage = ((TextMessage)message).getText();
 
