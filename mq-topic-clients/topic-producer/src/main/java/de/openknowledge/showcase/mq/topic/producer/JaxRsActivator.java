48df69c6dc9d412f948818323fd0997177ea7f81
max.weis <max.weis@openknowledge.de>
2021-06-16 09:01:34 +0200

    Add AsyncAPI Annotations and Document

--- mq-topic-clients/topic-producer/src/main/java/de/openknowledge/showcase/mq/topic/producer/JaxRsActivator.java
+++ mq-topic-clients/topic-producer/src/main/java/de/openknowledge/showcase/mq/topic/producer/JaxRsActivator.java
@@ -15,12 +15,47 @@
  */
 package de.openknowledge.showcase.mq.topic.producer;
 
+import io.smallrye.asyncapi.spec.annotations.AsyncAPI;
+import io.smallrye.asyncapi.spec.annotations.info.Info;
+import io.smallrye.asyncapi.spec.annotations.info.License;
+import io.smallrye.asyncapi.spec.annotations.server.Server;
+import io.smallrye.asyncapi.spec.annotations.server.ServerVariable;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
  * JAX-RS Activator
  */
+@AsyncAPI(
+    asyncapi = "2.0.0",
+    defaultContentType = "plain/text",
+    info = @Info(
+        title = "JMS Topic Producer API",
+        version = "0",
+        description = "A REST API to send messages via JMS to a MQ Broker",
+        license = @License(
+            name = "Apache 2.0",
+            url = "https://www.apache.org/licenses/LICENSE-2.0"
+        )
+    ),
+    id = "urn:de:openknowledge:topic:producer:server",
+    servers = {
+        @Server(
+            url = "localhost:{port}",
+            protocol = "jms",
+            description = "Test broker",
+            name = "dev",
+            variables = {
+                @ServerVariable(
+                    name = "port",
+                    description = "Connection is available through port 1414.",
+                    defaultValue = "1414"
+                )
+            }
+        )
+    }
+)
 @ApplicationPath("api")
 public class JaxRsActivator extends Application {
 
